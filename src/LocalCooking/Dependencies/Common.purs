cf7df0ad1a5a2d7d9146ac67579ce9d3d65904a3
Athan Clark <athan.clark@gmail.com>
2018-05-30 07:57:41 -0600

    fixed get user

--- src/LocalCooking/Dependencies/Common.purs
+++ src/LocalCooking/Dependencies/Common.purs
@@ -1,10 +1,16 @@
 module LocalCooking.Dependencies.Common where
 
-import LocalCooking.Semantics.Common (Register)
-import Data.Argonaut.JSONUnit (JSONUnit)
+import LocalCooking.Dependencies.AccessToken.Generic (AccessInitIn)
+import LocalCooking.Semantics.Common (Register, User)
+import LocalCooking.Common.AccessToken.Auth (AuthToken)
 
 import Sparrow.Client.Queue (SparrowStaticClientQueues)
 
+import Data.Argonaut.JSONUnit (JSONUnit)
 
-type RegisterSparrowClientQueues eff = SparrowStaticClientQueues eff
-  Register JSONUnit
+
+type RegisterSparrowClientQueues eff =
+  SparrowStaticClientQueues eff Register JSONUnit
+
+type GetUserSparrowClientQueues eff =
+  SparrowStaticClientQueues eff (AccessInitIn AuthToken JSONUnit) User
