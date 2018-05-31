557d4e1955fca2bc32e4d5e84309c939e7f4d6c9
Athan Clark <athan.clark@gmail.com>
2018-05-31 14:12:36 -0600

    damn son! First collected deps

--- src/LocalCooking/Dependencies/Admin.purs
+++ src/LocalCooking/Dependencies/Admin.purs
@@ -4,24 +4,54 @@ import LocalCooking.Dependencies.AccessToken.Generic (AccessInitIn)
 import LocalCooking.Common.AccessToken.Auth (AuthToken)
 import LocalCooking.Semantics.Common (User, Register)
 
-import Sparrow.Client.Queue (SparrowStaticClientQueues)
+import Sparrow.Client (unpackClient)
+import Sparrow.Client.Types (SparrowClientT)
+import Sparrow.Client.Queue (SparrowStaticClientQueues, sparrowStaticClientQueues)
+import Sparrow.Types (Topic (..))
 
+import Prelude
 import Data.Date (Date)
 import Data.Date.JSON (JSONDate (..))
 import Data.String.Permalink (Permalink)
 import Data.Argonaut.JSONUnit (JSONUnit)
 import Data.Argonaut (class EncodeJson, class DecodeJson, (:=), (.?), (~>), jsonEmptyObject, decodeJson, fail)
 import Data.Generic (class Generic)
+import Data.Functor.Singleton (class SingletonFunctor)
+import Control.Monad.Trans.Control (class MonadBaseControl)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Ref (REF)
+import Control.Monad.Eff.Class (class MonadEff)
+import Control.Monad.Eff.Exception (EXCEPTION)
 import Text.Email.Validate (EmailAddress)
+
+
+
+type Effects eff =
+  ( ref :: REF
+  , exception :: EXCEPTION
+  | eff)
+
+
+adminDependencies :: forall eff stM m
+                   . MonadBaseControl (Eff (Effects eff)) m stM
+                  => MonadEff (Effects eff) m
+                  => SingletonFunctor stM
+                  => { getUsersQueues :: GetUsersSparrowClientQueues (Effects eff)
+                     , setUserQueues  :: SetUserSparrowClientQueues (Effects eff)
+                     , newUserQueues  :: NewUserSparrowClientQueues (Effects eff)
+                     }
+                  -> SparrowClientT (Effects eff) m Unit
+adminDependencies {getUsersQueues,setUserQueues,newUserQueues} = do
+  unpackClient (Topic ["admin","getUsers"]) (sparrowStaticClientQueues getUsersQueues)
 
 
 type GetUsersSparrowClientQueues eff =
   SparrowStaticClientQueues eff (AccessInitIn AuthToken JSONUnit) (Array User)
 
 
-type SetUsersSparrowClientQueues eff =
+type SetUserSparrowClientQueues eff =
   SparrowStaticClientQueues eff (AccessInitIn AuthToken User) JSONUnit
 
 
-type NewUsersSparrowClientQueues eff =
+type NewUserSparrowClientQueues eff =
   SparrowStaticClientQueues eff (AccessInitIn AuthToken Register) JSONUnit
--- src/LocalCooking/Dependencies/AuthToken.purs
+++ src/LocalCooking/Dependencies/AuthToken.purs
@@ -6,14 +6,40 @@ import LocalCooking.Semantics.Common (Login, SocialLogin)
 import LocalCooking.Common.AccessToken.Auth (AuthToken)
 import Facebook.Types (FacebookUserId, FacebookLoginReturnError)
 
-import Sparrow.Client.Queue (SparrowClientQueues)
+import Sparrow.Client (unpackClient)
+import Sparrow.Client.Types (SparrowClientT)
+import Sparrow.Client.Queue (SparrowClientQueues, sparrowClientQueues)
+import Sparrow.Types (Topic (..))
+
 import Prelude
 import Data.Maybe (Maybe (..))
 import Data.Either (Either (..))
 import Data.Newtype (unwrap, wrap)
 import Data.Argonaut (class EncodeJson, class DecodeJson, (:=), (.?), (~>), jsonEmptyObject, decodeJson, fail, encodeJson)
+import Data.Functor.Singleton (class SingletonFunctor)
+import Control.Monad.Trans.Control (class MonadBaseControl)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Class (class MonadEff)
+import Control.Monad.Eff.Ref (REF)
+import Control.Monad.Eff.Exception (EXCEPTION)
 import Control.Alternative ((<|>))
 
+
+type Effects eff =
+  ( ref :: REF
+  , exception :: EXCEPTION
+  | eff)
+
+
+authTokenDependencies :: forall eff stM m
+                       . MonadBaseControl (Eff (Effects eff)) m stM
+                      => MonadEff (Effects eff) m
+                      => SingletonFunctor stM
+                      => { authTokenQueues :: AuthTokenSparrowClientQueues (Effects eff)
+                         }
+                      -> SparrowClientT (Effects eff) m Unit
+authTokenDependencies {authTokenQueues} = do
+  unpackClient (Topic ["authToken"]) (sparrowClientQueues authTokenQueues)
 
 
 data AuthTokenInitIn
