a9976671442c77c3d690eb3a0f2308a204b4e36d
Athan Clark <athan.clark@gmail.com>
2018-05-31 08:36:20 -0600

    more mitch impl

--- src/LocalCooking/Dependencies/Mitch.purs
+++ src/LocalCooking/Dependencies/Mitch.purs
@@ -4,8 +4,10 @@ module LocalCooking.Dependencies.Mitch where
 import LocalCooking.Dependencies.AccessToken.Generic (AccessInitIn)
 import LocalCooking.Common.Rating (Rating)
 import LocalCooking.Common.AccessToken.Auth (AuthToken)
-import LocalCooking.Semantics.Mitch (Customer)
-import LocalCooking.Database.Schema (StoredOrderId, StoredReviewId)
+import LocalCooking.Semantics.Mitch
+  ( Customer, MealSynopsis, MenuSynopsis, ChefSynopsis, Meal, Menu, Chef, Review)
+import LocalCooking.Database.Schema
+  ( StoredOrderId, StoredReviewId, StoredMealId, StoredMenuId, StoredChefId)
 
 import Sparrow.Client.Queue (SparrowStaticClientQueues)
 
@@ -48,3 +50,41 @@ instance encodeJsonSubmitReview :: EncodeJson SubmitReview where
 
 type SubmitReviewSparrowClientQueues eff =
   SparrowStaticClientQueues eff (AccessInitIn AuthToken SubmitReview) StoredReviewId
+
+
+type GetReviewSparrowClientQueues eff =
+  SparrowStaticClientQueues eff StoredReviewId Review
+
+
+type GetMealSynopsisSparrowClientQueues eff =
+  SparrowStaticClientQueues eff StoredMealId MealSynopsis
+
+
+type GetChefSynopsisSparrowClientQueues eff =
+  SparrowStaticClientQueues eff StoredChefId ChefSynopsis
+
+
+type GetChefMenuSynopsesSparrowClientQueues eff =
+  SparrowStaticClientQueues eff StoredChefId (Array MenuSynopsis)
+
+
+type GetMenuMealSynopsesSparrowClientQueues eff =
+  SparrowStaticClientQueues eff StoredMenuId (Array MealSynopsis)
+
+
+type BrowseChefSparrowClientQueues eff =
+  SparrowStaticClientQueues eff Permalink Chef
+
+
+newtype BrowseMenu = BrowseMenu
+  { chef :: Permalink
+  , deadline :: Date
+  }
+
+derive instance genericBrowseMenu :: Generic BrowseMenu
+
+instance encodeJsonBrowseMenu :: EncodeJson BrowseMenu where
+  encodeJson (BrowseMenu {chef,deadline})
+    =  "chef" := chef
+    ~> "deadline" := JSONDate deadline
+    ~> jsonEmptyObject
