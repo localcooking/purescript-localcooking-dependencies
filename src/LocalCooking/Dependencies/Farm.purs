2fa3a1c6f57a2b41514c79e27f5fa35e37aa6d43
Athan Clark <athan.clark@gmail.com>
2018-06-20 15:08:05 -0600

    hey at least my life is worth something

---
+++ src/LocalCooking/Dependencies/Farm.purs
@@ -0,0 +1,83 @@
+module LocalCooking.Dependencies.Farm where
+
+import LocalCooking.Dependencies.AccessToken.Generic (AccessInitIn)
+import LocalCooking.Common.AccessToken.Auth (AuthToken)
+import LocalCooking.Common.Tag.Meal (MealTag)
+-- import LocalCooking.Common.Tag.Farm (FarmTag)
+import LocalCooking.Semantics.Farm ()
+import LocalCooking.Database.Schema (StoredMealId, StoredMenuId)
+
+import Sparrow.Client (unpackClient)
+import Sparrow.Client.Types (SparrowClientT)
+import Sparrow.Client.Queue (SparrowStaticClientQueues, sparrowStaticClientQueues, newSparrowStaticClientQueues)
+import Sparrow.Types (Topic (..))
+
+import Prelude
+import Data.Argonaut.JSONUnit (JSONUnit)
+import Data.Argonaut (class EncodeJson, class DecodeJson, (:=), (.?), (~>), jsonEmptyObject, decodeJson)
+import Data.Generic (class Generic)
+import Data.Functor.Singleton (class SingletonFunctor)
+import Control.Monad.Trans.Control (class MonadBaseControl)
+import Control.Monad.Eff (Eff, kind Effect)
+import Control.Monad.Eff.Ref (REF)
+import Control.Monad.Eff.Class (class MonadEff)
+import Control.Monad.Eff.Exception (EXCEPTION)
+
+
+
+type Effects eff =
+  ( ref :: REF
+  , exception :: EXCEPTION
+  | eff)
+
+
+type FarmQueues (eff :: # Effect) =
+  {}
+
+
+newFarmQueues :: forall eff. Eff (Effects eff) (FarmQueues (Effects eff))
+newFarmQueues = do
+  -- addMealTagQueues <- newSparrowStaticClientQueues
+  -- addFarmTagQueues <- newSparrowStaticClientQueues
+  -- getFarmQueues <- newSparrowStaticClientQueues
+  -- setFarmQueues <- newSparrowStaticClientQueues
+  -- getMenusQueues <- newSparrowStaticClientQueues
+  -- setMenuQueues <- newSparrowStaticClientQueues
+  -- newMenuQueues <- newSparrowStaticClientQueues
+  -- getMealsQueues <- newSparrowStaticClientQueues
+  -- setMealQueues <- newSparrowStaticClientQueues
+  -- newMealQueues <- newSparrowStaticClientQueues
+  pure {}
+    -- { addMealTagQueues
+    -- , addFarmTagQueues
+    -- , getFarmQueues
+    -- , setFarmQueues
+    -- , getMenusQueues
+    -- , setMenuQueues
+    -- , newMenuQueues
+    -- , getMealsQueues
+    -- , setMealQueues
+    -- , newMealQueues
+    -- }
+
+
+farmDependencies :: forall eff stM m
+                      . MonadBaseControl (Eff (Effects eff)) m stM
+                     => MonadEff (Effects eff) m
+                     => SingletonFunctor stM
+                     => FarmQueues (Effects eff)
+                     -> SparrowClientT (Effects eff) m Unit
+farmDependencies
+  {} = do
+  pure unit
+  -- unpackClient (Topic ["farm","addMealTag"]) (sparrowStaticClientQueues addMealTagQueues)
+  -- unpackClient (Topic ["farm","addFarmTag"]) (sparrowStaticClientQueues addFarmTagQueues)
+  -- unpackClient (Topic ["farm","getFarm"]) (sparrowStaticClientQueues getFarmQueues)
+  -- unpackClient (Topic ["farm","setFarm"]) (sparrowStaticClientQueues setFarmQueues)
+  -- unpackClient (Topic ["farm","getMenus"]) (sparrowStaticClientQueues getMenusQueues)
+  -- unpackClient (Topic ["farm","setMenu"]) (sparrowStaticClientQueues setMenuQueues)
+  -- unpackClient (Topic ["farm","newMenu"]) (sparrowStaticClientQueues newMenuQueues)
+  -- unpackClient (Topic ["farm","getMeals"]) (sparrowStaticClientQueues getMealsQueues)
+  -- unpackClient (Topic ["farm","setMeal"]) (sparrowStaticClientQueues setMealQueues)
+  -- unpackClient (Topic ["farm","newMeal"]) (sparrowStaticClientQueues newMealQueues)
+
---
+++ src/LocalCooking/Dependencies/Restaurant.purs
@@ -0,0 +1,82 @@
+module LocalCooking.Dependencies.Restaurant where
+
+import LocalCooking.Dependencies.AccessToken.Generic (AccessInitIn)
+import LocalCooking.Common.AccessToken.Auth (AuthToken)
+import LocalCooking.Common.Tag.Meal (MealTag)
+-- import LocalCooking.Common.Tag.Restaurant (RestaurantTag)
+import LocalCooking.Semantics.Restaurant ()
+import LocalCooking.Database.Schema (StoredMealId, StoredMenuId)
+
+import Sparrow.Client (unpackClient)
+import Sparrow.Client.Types (SparrowClientT)
+import Sparrow.Client.Queue (SparrowStaticClientQueues, sparrowStaticClientQueues, newSparrowStaticClientQueues)
+import Sparrow.Types (Topic (..))
+
+import Prelude
+import Data.Argonaut.JSONUnit (JSONUnit)
+import Data.Argonaut (class EncodeJson, class DecodeJson, (:=), (.?), (~>), jsonEmptyObject, decodeJson)
+import Data.Generic (class Generic)
+import Data.Functor.Singleton (class SingletonFunctor)
+import Control.Monad.Trans.Control (class MonadBaseControl)
+import Control.Monad.Eff (Eff, kind Effect)
+import Control.Monad.Eff.Ref (REF)
+import Control.Monad.Eff.Class (class MonadEff)
+import Control.Monad.Eff.Exception (EXCEPTION)
+
+
+
+type Effects eff =
+  ( ref :: REF
+  , exception :: EXCEPTION
+  | eff)
+
+
+type RestaurantQueues (eff :: # Effect) =
+  {}
+
+
+newRestaurantQueues :: forall eff. Eff (Effects eff) (RestaurantQueues (Effects eff))
+newRestaurantQueues = do
+  -- addMealTagQueues <- newSparrowStaticClientQueues
+  -- addRestaurantTagQueues <- newSparrowStaticClientQueues
+  -- getRestaurantQueues <- newSparrowStaticClientQueues
+  -- setRestaurantQueues <- newSparrowStaticClientQueues
+  -- getMenusQueues <- newSparrowStaticClientQueues
+  -- setMenuQueues <- newSparrowStaticClientQueues
+  -- newMenuQueues <- newSparrowStaticClientQueues
+  -- getMealsQueues <- newSparrowStaticClientQueues
+  -- setMealQueues <- newSparrowStaticClientQueues
+  -- newMealQueues <- newSparrowStaticClientQueues
+  pure {}
+    -- { addMealTagQueues
+    -- , addRestaurantTagQueues
+    -- , getRestaurantQueues
+    -- , setRestaurantQueues
+    -- , getMenusQueues
+    -- , setMenuQueues
+    -- , newMenuQueues
+    -- , getMealsQueues
+    -- , setMealQueues
+    -- , newMealQueues
+    -- }
+
+
+restaurantDependencies :: forall eff stM m
+                      . MonadBaseControl (Eff (Effects eff)) m stM
+                     => MonadEff (Effects eff) m
+                     => SingletonFunctor stM
+                     => RestaurantQueues (Effects eff)
+                     -> SparrowClientT (Effects eff) m Unit
+restaurantDependencies
+  {} = do
+  pure unit
+  -- unpackClient (Topic ["restaurant","addMealTag"]) (sparrowStaticClientQueues addMealTagQueues)
+  -- unpackClient (Topic ["restaurant","addRestaurantTag"]) (sparrowStaticClientQueues addRestaurantTagQueues)
+  -- unpackClient (Topic ["restaurant","getRestaurant"]) (sparrowStaticClientQueues getRestaurantQueues)
+  -- unpackClient (Topic ["restaurant","setRestaurant"]) (sparrowStaticClientQueues setRestaurantQueues)
+  -- unpackClient (Topic ["restaurant","getMenus"]) (sparrowStaticClientQueues getMenusQueues)
+  -- unpackClient (Topic ["restaurant","setMenu"]) (sparrowStaticClientQueues setMenuQueues)
+  -- unpackClient (Topic ["restaurant","newMenu"]) (sparrowStaticClientQueues newMenuQueues)
+  -- unpackClient (Topic ["restaurant","getMeals"]) (sparrowStaticClientQueues getMealsQueues)
+  -- unpackClient (Topic ["restaurant","setMeal"]) (sparrowStaticClientQueues setMealQueues)
+  -- unpackClient (Topic ["restaurant","newMeal"]) (sparrowStaticClientQueues newMealQueues)
